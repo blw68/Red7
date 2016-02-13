68e637e0e0dec2d17e210220018926d364f1b73c
blw68 <blw68@scarletmail.rutgers.edu>
2016-02-13 12:09:54 -0500

    Create Card.java

---
+++ Card.java
@@ -0,0 +1,13 @@
+public class Card {
+	
+	int color;
+	int num;
+	Card next;
+	
+	public Card(int color, int num, Card next) {
+		this.color = color;
+		this.num = num;
+		this.next = next;
+	}
+
+}
